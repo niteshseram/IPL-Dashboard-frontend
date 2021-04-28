34b63745b90e0e48a1165efe27b797f4094bab25
S Nitesh Singh <niteshseram@gmail.com>
2021-04-28 19:45:51 +0530

    intial commit

---
+++ src/components/MatchDetailCard.js
@@ -0,0 +1,9 @@
+import { React } from "react";
+
+export const MatchDetailCard = () => {
+  return (
+    <div className="MatchDetailCard">
+      <h1>IPL Dashboard</h1>
+    </div>
+  );
+};
---
+++ src/pages/TeamPage.js
@@ -0,0 +1,13 @@
+import { React } from "react";
+import { MatchDetailCard } from "../components/MatchDetailCard";
+import { MatchSmallCard } from "../components/MatchSmallCard";
+
+export const TeamPage = () => {
+  return (
+    <div className="TeamPage">
+      <h1>Team Name</h1>
+      <MatchDetailCard />
+      <MatchSmallCard />
+    </div>
+  );
+};
